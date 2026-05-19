ed66afee30b4beba32c142359c61ea680a82206e
Lalo Valenzuela <lalo.valenz.glez@gmail.com>
2026-05-18 23:23:10 -0600

    style(devops): configurar lint-staged y dotnet format para pre-commits

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -7,9 +7,14 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment())
     app.MapOpenApi();
-}
+
+
+
+
+
+
 
 app.UseHttpsRedirection();
 
